a04a3f88e3cedd34c4c43aedd7b25398890a6769
Herni <hernanatn@gmail.com>
2025-03-14 13:43:28 -0300

    [AG]: commit inicial

---
+++ fuente/utiles_google/__init__.py
@@ -0,0 +1,18 @@
+"""
+===============
+chastack-utiles-google
+===============
+Librería de útiles para consumo de APIS de google en Python 3.
+
+* Repositorio del proyecto: https://github.com/Hernanatn/chastack/utiles_google
+
+Derechos de autor (c) 2025 Ch'aska SRL. Distribuído bajo licencia MIT.
+Autores:
+ - Hernan ATN | herni@cajadeideas.ar 
+"""
+import utiles_google.recurso
+import utiles_google.correo
+import utiles_google.hojas_de_calculo
+
+if __name__ == '__main__':
+    print(__doc__)
